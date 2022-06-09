d8ffe55da5d716d2f4e445aca76ad796f23f711d
Madison Kendall <lilyzetakovic@gmail.com>
2022-06-09 13:08:38 -0600

    Add helper functions

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <section>
-
-
+       <div id="about">
+          <h1 className="page-header">About Me</h1>
+       </div>
+       <div>
+          <img
+             src={require("../../assets/images/")}
+             alt="about-me-photo"
+          />
+      </div>
+      <div>
+         <p>
+            Hi! Welcome to my portfolio!
+             I am a full stack web developer based in Denver, Colorado.
+             I love minimal design. I love running, singing, and golfing in my free time.
+         </p>
+      </div>
     </section>
   );
 }
