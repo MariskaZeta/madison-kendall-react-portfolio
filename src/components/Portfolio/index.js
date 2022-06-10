f920e73598b98cbc6be57b63df3f11a1c92b8ca6
Madison Kendall <lilyzetakovic@gmail.com>
2022-06-10 11:37:47 -0600

    add in checkBlank helper function

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import Project from "../Project";
 
 function Portfolio() {
-  // const projects = [
-  //   {
-  //     name: "It Takes A City",
-  //     description: "",
-  //     image: "",
-  //     technologies: [
-  //
-  //     ],
-  //     github: "",
-  //
-  //   }
-  // ]
+  const projects = [
+    {
+      name: "It Takes A City",
+      description: "This application was created with ",
+      image: "",
+      technologies: [
+  
+      ],
+      github: "",
+
+    }
+  ]
   return (
 
   )
