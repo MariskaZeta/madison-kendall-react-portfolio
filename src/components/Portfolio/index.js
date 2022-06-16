56e7f8dbfe52f72a293e3b1fe316427b57e2eea2
Madison Kendall <lilyzetakovic@gmail.com>
2022-06-16 06:29:58 -0600

    Add Portfolio structure

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,21 +2,92 @@ import React from "react";
 import Project from "../Project";
 
 function Portfolio() {
+  // storing all the projects I've done in JSON objects
   const projects = [
     {
-      name: "It Takes A City",
-      description: "This application was created with ",
-      image: "",
+      name: "It Takes a City",
+      description: "This application was created to..",
+      image: ,
       technologies: [
-  
+        "HTML/CSS",
+        "JavaScript",
+        "Express.js",
+        "MySQL2",
+        "dotenv",
+        "Tailwind",
+        "Auth0",
+        "EJS"
       ],
-      github: "",
-
-    }
+      github: "https://github.com/cwarmbrunn/it-takes-a-city",
+      deployed: "https://it-takes-a-city.herokuapp.com/"
+    },
+    {
+      name: "It Takes a City",
+      description: "This application was created to..",
+      image: ,
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Express.js",
+        "MySQL2",
+        "dotenv",
+        "Tailwind",
+        "Auth0",
+        "EJS"
+      ],
+      github: "https://github.com/cwarmbrunn/it-takes-a-city",
+      deployed: "https://it-takes-a-city.herokuapp.com/"
+    },
+    {
+      name: "It Takes a City",
+      description: "This application was created to..",
+      image: ,
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Express.js",
+        "MySQL2",
+        "dotenv",
+        "Tailwind",
+        "Auth0",
+        "EJS"
+      ],
+      github: "https://github.com/cwarmbrunn/it-takes-a-city",
+      deployed: "https://it-takes-a-city.herokuapp.com/"
+    },
   ]
   return (
-
-  )
+    <section>
+        <div className="center">
+            <h1 className="page-header">My Portfolio</h1>
+        </div>
+            <ul className="flex-row mobile-row">
+                <li className="padding">
+                     <Project projects={projects[0]}></Project>
+                </li>
+                <li className="padding">
+                    <Project projects={projects[1]}></Project>
+                </li>
+            </ul>
+            <ul className="flex-row mobile-row">
+                <li className="padding">
+                    <Project projects={projects[2]}></Project>
+                </li>
+                <li className="padding">
+                    <Project projects={projects[3]}></Project>
+                </li>
+            </ul>
+            <ul className="flex-row mobile-row">
+                <li className="padding">
+                    <Project projects={projects[4]}></Project>
+                </li>
+                <li className="padding">
+                    <Project projects={projects[5]}></Project>
+                </li>
+            </ul>
+        </div>
+    </section>
+  );
 }
 
 export default Portfolio;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -29,7 +29,7 @@ function Resume() {
                      <li>Progressive Web Applications (PWA) </li>
                  </ol>
                <br></br>
-                 <h5>Database Proficiencies<h5>
+                 <h5>Database Proficiencies</h5>
                  <ol>
                      <li>MySQL</li>
                      <li>Sequelize</li>
