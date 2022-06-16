dc1fb25d084ffa63d05a792585af27fcaa59bf2f
Madison Kendall <lilyzetakovic@gmail.com>
2022-06-16 13:59:00 -0600

    Add styling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,7 +5,7 @@ function Portfolio() {
   // storing all the projects I've done in JSON objects
   const projects = [
     {
-      name: "It Takes a City",
+      name: "It Takes A City",
       description: "An application that provides information for people in need of resources (e.g. food, shelters, etc.).",
       image:"ItTakesACity-Screenshot2.png",
       technologies: [
@@ -16,7 +16,8 @@ function Portfolio() {
         "dotenv",
         "Tailwind",
         "Auth0",
-        "EJS"
+        "EJS",
+        "Heroku"
       ],
       github: "https://github.com/cwarmbrunn/it-takes-a-city",
       deployed: "https://it-takes-a-city.herokuapp.com/",
@@ -35,7 +36,6 @@ function Portfolio() {
         "Insomnia"
       ],
       github: "https://github.com/MariskaZeta/social-network-app-pro",
-      deployed: "https://it-takes-a-city.herokuapp.com/",
     },
     {
       name: "Tech Blog Pro",
@@ -52,11 +52,55 @@ function Portfolio() {
         "bcrypt package",
         "express-session package",
         "connect-session package",
-        "dotenv package"
+        "dotenv package",
+        "Heroku"
       ],
       github: "https://github.com/MariskaZeta/tech-blog-pro",
       deployed: "https://tech-blog-pro-mvc.herokuapp.com/",
-    }
+    },
+    {
+      name: "e-commerce company back-end",
+      description: "This application is the back-end of an e-commerce site. Using an Express.js API that uses Sequelize to interact with the data inside of the MySQL database.",
+      image: "ItTakesACity-Screenshot2.png",
+      technologies: [
+        "Express",
+        "MySQL2",
+        "Sequelize",
+        "Node.js",
+        "Insomnia",
+        "dotenv",
+        "Npm"
+      ],
+      github: "https://github.com/MariskaZeta/e-commerce-company-back-end",
+    },
+    {
+      name: "Note Taker Pro",
+      description: "This application can be used to write, save, and delete notes. It uses an Express.js back end and it saves and retrieves note data from a JSON file.",
+      image: "ItTakesACity-Screenshot2.png",
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Node.js",
+        "Express.js",
+        "Bootstrap",
+        "Heroku"
+      ],
+      github: "https://github.com/MariskaZeta/note-taker-pro",
+      deployed: "https://thawing-forest-88059.herokuapp.com/",
+    },
+    {
+      name: "Auto Dev Team Profiler",
+      description: "This application is a back end application using Node.js. It uses the inquirer npm package to get the user input to dynamically create a team profile. Users will go through questions about the type of employee that will be added to the team.",
+      image: "ItTakesACity-Screenshot2.png",
+      technologies: [
+        "HTML/CSS",
+        "JavaScript",
+        "Node.js",
+        "Jest",
+        "Inquirer npm package"
+      ],
+      github: "https://github.com/MariskaZeta/auto-dev-team-profiler",
+    },
   ];
 
   return (
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -38,8 +38,9 @@ function Project(props) {
                 <Card.Body>
                     <Card.Title className="card-title">{name}</Card.Title>
                     <Card.Text className="card-text">{description}</Card.Text>
-                    <Card.Subtitle className="card-subtitle">Techs Used</Card.Subtitle>
+                    <Card.Subtitle className="card-subtitle">Technologies Used:</Card.Subtitle>
                     <Card.Text className="card-techs">{getTechs(techs)}</Card.Text>
+                    
                     <Card.Link href={appLink} target="_blank" className="card-link">
                     {name} App
                     </Card.Link>
